c634f09f2b670159ff8d33632a8e6e3f2bbaf70b
MEIYAZHAGAN <96281339+MEIYAZHAGANgit@users.noreply.github.com>
2022-01-27 11:05:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const countdown = document.querySelector(".countdown");
+ let time =10;
+
+countdown.innerHTML = time--;
+setTimeout(() => {
+    countdown.innerHTML = time--;
+setTimeout(() => {
+    countdown.innerHTML= time--;
+    setTimeout(() => {
+        countdown.innerHTML = time--;
+    setTimeout(() => {
+        countdown.innerHTML= time--;
+        setTimeout(() => {
+            countdown.innerHTML = time--;
+        setTimeout(() => {
+            countdown.innerHTML= time--;
+            setTimeout(() => {
+                countdown.innerHTML = time--;
+            setTimeout(() => {
+                countdown.innerHTML= time--;
+                setTimeout(() => {
+                    countdown.innerHTML = time--;
+                setTimeout(() => {
+                    countdown.innerHTML = "Happy Republic Day🥳🎊🇮🇳";
+                
+                },1000);
+                },1000);    
+            },1000);
+            },1000);    
+        },1000);
+        },1000);
+    },1000);
+    },1000);    
+},1000);
+},1000);
+
